14681ae061fb42ea6ef662d492566b7cb457c814
SfirleaAndreiBogdan <sfirleaandrei25@gmail.com>
2024-11-11 22:36:11 +0200

    final lab6

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -119,57 +119,80 @@ namespace Sfirlea_Andrei_Bogdan_Lab2.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
-            ExternalLogins = (await
-           _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            if (!ModelState.IsValid)
+            {
+                // Dacă datele de intrare nu sunt valide, returnăm pagina curentă
+                return Page();
+            }
 
             var user = CreateUser();
-            await _userStore.SetUserNameAsync(user, Input.Email,
-           CancellationToken.None);
-            await _emailStore.SetEmailAsync(user, Input.Email,
-           CancellationToken.None);
-            var result = await _userManager.CreateAsync(user,
-           Input.Password);
-            Member.Email = Input.Email;
-            _context.Member.Add(Member);
-            await _context.SaveChangesAsync();
+            await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+            await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+          
+            var result = await _userManager.CreateAsync(user, Input.Password);
+
             if (result.Succeeded)
             {
                 _logger.LogInformation("User created a new account with password.");
+
+               
+                await _userManager.AddToRoleAsync(user, "User");
+
+               
                 var userId = await _userManager.GetUserIdAsync(user);
-                var code = await
-               _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code =
-               WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                "/Account/ConfirmEmail",
-               pageHandler: null,
-               values: new
-               {
-                   area = "Identity",
-                   userId = userId,
-                   code = code,
-                   returnUrl = returnUrl
-               },
-                protocol: Request.Scheme);
-                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email", $"Please confirm your account by <a href = '{HtmlEncoder.Default.Encode(callbackUrl)}' > clicking here </ a >.");
 
-
-                if(_userManager.Options.SignIn.RequireConfirmedAccount)
+             
+                int userIdInt;
+                if (int.TryParse(userId, out userIdInt))
                 {
-                    return RedirectToPage("RegisterConfirmation", new
-                    {
-                        email = Input.Email,
-                        returnUrl = returnUrl
-                    });
+                    Member.ID = userIdInt;
                 }
                 else
                 {
-                    await _signInManager.SignInAsync(user,
-                   isPersistent: false);
+                    _logger.LogError("Failed to convert userId to int.");
+                }
+                Member.Email = Input.Email;
+
+               
+                _context.Member.Add(Member);
+                await _context.SaveChangesAsync();
+
+                
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+                var callbackUrl = Url.Page(
+                    "/Account/ConfirmEmail",
+                    pageHandler: null,
+                    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                    protocol: Request.Scheme);
+
+              
+                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                
+                if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                {
+                    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                }
+                else
+                {
+                    
+                    await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnUrl);
                 }
-
             }
+
+           
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
             return Page();
         }
 
--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ using Sfirlea_Andrei_Bogdan_Lab2.Models;
 
 namespace Sfirlea_Andrei_Bogdan_Lab2.Pages.Books
 {
+    [Authorize(Roles = "Admin")]
     public class EditModel : BookCategoriesPageModel
     {
         private readonly Sfirlea_Andrei_Bogdan_Lab2Context _context;
